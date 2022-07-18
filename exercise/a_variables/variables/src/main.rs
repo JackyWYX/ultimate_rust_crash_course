cf5919629605f916437eb77d93c8d10c1bd46d7b
jacky <jackyw.se@gmail.com>
2022-07-17 20:00:39 -0700

    add first exercise

---
+++ exercise/a_variables/variables/src/main.rs
@@ -0,0 +1,14 @@
+const STARTING_MISSLES: i32 = 8;
+const READY_AMOUNT:i32 = 2;
+
+fn main() {
+    // let mut missiles = STARTING_MISSLES;
+    // let ready = READY_AMOUNT;
+    let (mut missiles, ready) = (STARTING_MISSLES, READY_AMOUNT);
+    println!("Firing {} of my {} missiles...", ready, missiles);
+
+    // missiles = missiles - ready;
+    println!("{} missiles left", missiles - ready);
+
+    // let var_not_used = STARTING_MISSLES;
+}
